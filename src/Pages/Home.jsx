bbe275688f36ff095e67efd922015cee2e8e7388
masonprotsman <masonprotsman@gmail.com>
2025-11-20 21:33:53 -0500

    bg dark always no toggle

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,3 @@
-import { ThemeToggle } from '../components/ThemeToggle'
 import { StarBackground } from '../components/StarBackground'
 import { NavBar } from '../components/NavBar'
 import { HeroSection } from '../components/HeroSection'
@@ -11,9 +10,6 @@ import { Footer } from '../components/Footer'
 const Home = () => {
     return (
         <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
-
-            {/* theme toggle*/}
-            <ThemeToggle />
 
             {/* background effects */}
             <StarBackground />
